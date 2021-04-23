8ab8f5690b289f10757841fd0bb5bcbb963edeeb
CheneyZhe <1438365611@qq.com>
2021-04-23 08:48:34 +0800

    Envoy

---
+++ Web.MVC/Services/GatewayService.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Web.MVC.Services
+{
+    public class GatewayService : IServiceBase
+    {
+        private readonly IConfiguration configuration;
+
+        public GatewayService(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public async Task<string> GetCYOranizationAsync()
+        {
+            string baseUri = configuration["GatewayBaseUri"];
+            var client = new HttpClient();
+             
+            var result = await client.GetAsync(baseUri + "/cyoranization");
+
+            return await result.Content.ReadAsStringAsync();
+
+        }
+
+        public async Task<string> GetCYPlanningAsync()
+        {
+            string baseUri = configuration["GatewayBaseUri"];
+            var client = new HttpClient();
+            var result = await client.GetAsync(baseUri + "/cyplanning");
+
+            return await result.Content.ReadAsStringAsync();
+        }
+
+        public void GetServices()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
